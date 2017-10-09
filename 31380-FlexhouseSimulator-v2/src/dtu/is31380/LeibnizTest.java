ec4487c2d514672601a97e56a430f1a923498180
EdvardMyhre <edvardflm@gmail.com>
2017-10-09 12:56:00 +0200

    Add leibniz test from ex 6

---
+++ 31380-FlexhouseSimulator-v2/src/dtu/is31380/LeibnizTest.java
@@ -0,0 +1,17 @@
+package dtu.is31380;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class LeibnizTest {
+
+	@Test
+	public void test() {
+		// assertEquals(3.14, Leibniz.pi(1000));
+		double d = Leibniz.pi(10000);
+		System.out.printf("The pi calculation is %f%n", d);
+		assertEquals(3.14, d, 0.05);
+	}
+
+}
